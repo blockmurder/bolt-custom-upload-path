2a72c36b987e2ca39fc4d16f9b8cecb9c7452757
blockmurder <info@blockmurder.ch>
2017-08-25 00:05:42 +0000

    initial commit

---
+++ src/ExtensionNameExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\YourName\ExtensionName;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * ExtensionName extension class.
+ *
+ * @author Your Name <you@example.com>
+ */
+class ExtensionNameExtension extends SimpleExtension
+{
+}
